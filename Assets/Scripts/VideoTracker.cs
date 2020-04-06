4a1c0f832798df3e1274e5622bfe797f7b462cdd
TibiRaducanu <tibi.raducanu@gmail.com>
2020-04-06 17:24:25 +0300

    Added Pause/Play Buttons for the video
    Added Pause/Play Buttons for the video. Also added a slider to choose a specific frame of the video.

---
+++ Assets/Scripts/VideoTracker.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Video;
+using UnityEngine.EventSystems;
+using System;
+
+public class VideoTracker : MonoBehaviour, IPointerDownHandler, IPointerUpHandler
+{
+    public VideoPlayer video;
+    Slider tracker;
+    bool isSliding;
+    float cooldown;
+    float cooldownValue;
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        isSliding = true;
+    }
+
+    public void OnPointerUp(PointerEventData eventData)
+    {
+        cooldown = cooldownValue;
+        float currentFrame = tracker.value * video.frameCount;
+        video.frame = (long) Math.Floor(currentFrame);
+        isSliding = false;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        tracker = GetComponent<Slider>();
+        isSliding = false;
+        cooldown = 0f;
+        cooldownValue = 0.2f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!isSliding && cooldown < 0.01f)
+        {
+            tracker.value = (float)video.frame / (float)video.frameCount;
+        }
+
+        cooldown -= Time.deltaTime;
+        if (cooldown < 0f) cooldown = 0f;
+    }
+}

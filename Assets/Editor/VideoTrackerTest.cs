5beb53ce1a7709ef2616e4b84ed26b9059e5fba8
TibiRaducanu <tibi.raducanu@gmail.com>
2020-06-11 22:59:54 +0300

    Added tests for video
    Added tests for video

---
+++ Assets/Editor/VideoTrackerTest.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using NUnit.Framework;
+using UnityEngine.Video;
+using UnityEngine.UI;
+
+public class VideoTrackerTest : MonoBehaviour
+{
+    [Test]
+    public void CheckFrameRate()
+    {
+        var currentVideo = GameObject.FindObjectOfType<VideoPlayer>();
+        var currentSlider = GameObject.FindObjectOfType<Slider>();
+
+        if (currentVideo && currentSlider)
+        {
+            Assert.IsTrue(currentSlider.value - (float)currentVideo.frame / (float)currentVideo.frameCount < 0.2);
+        }
+    }
+}
